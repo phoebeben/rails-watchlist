6cc4e53473e48db3131aa3b749727867afbd4300
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-05-25 16:06:30 +0100

    added bookmarks controller and added new bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # root 'lists#index'
+  # get 'lists/new', to: 'lists#new'
+  # get 'lists/:id', to: 'lists#show', as: :list
+  # post 'lists', to: 'lists#create'
+  root 'lists#index'
+  resources :lists do
+    resources :bookmarks, only: %i[new create]
+  end
 end
